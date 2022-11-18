e4584c6efcc9dec0920364bcce91f2b96fea97c5
Nguyễn Minh Thái <nguyenminhthai3108@gmail.com>
2022-11-18 09:58:28 +0700

    ADD: Add new method to get all categories name as a string list

--- src/main/java/com/nashtech/rookies/java05/AssetManagement/controler/AssetController.java
+++ src/main/java/com/nashtech/rookies/java05/AssetManagement/controler/AssetController.java
@@ -6,13 +6,11 @@ import com.nashtech.rookies.java05.AssetManagement.dtos.response.AssetViewRespon
 import com.nashtech.rookies.java05.AssetManagement.entities.enums.AssetState;
 import com.nashtech.rookies.java05.AssetManagement.services.AssetService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(path = "/api/assets")
 public class AssetController {
