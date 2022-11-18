d7a865b7986c1f0523630d71ba1d8faf14c16c05
Nguyễn Minh Thái <nguyenminhthai3108@gmail.com>
2022-11-18 16:34:44 +0700

    FIX: Fix get all categories name when categories are null it should return an empty list of String. ADD: Write test case for CategoryServiceImpl

--- src/main/java/com/nashtech/rookies/java05/AssetManagement/services/impl/CategoryServiceImpl.java
+++ src/main/java/com/nashtech/rookies/java05/AssetManagement/services/impl/CategoryServiceImpl.java
@@ -3,12 +3,13 @@ package com.nashtech.rookies.java05.AssetManagement.services.impl;
 import com.nashtech.rookies.java05.AssetManagement.entities.Category;
 import com.nashtech.rookies.java05.AssetManagement.repository.CategoryRepository;
 import com.nashtech.rookies.java05.AssetManagement.services.CategoryService;
+import lombok.Builder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@Builder
 @Service
 public class CategoryServiceImpl implements CategoryService {
     @Autowired
@@ -17,9 +18,13 @@ public class CategoryServiceImpl implements CategoryService {
     public List<String> getAllCategoriesName() {
         List<Category> categories = categoryRepository.findAll();
         List<String> result = new ArrayList<>();
-        categories.forEach(category -> {
-            result.add(category.getName());
-        });
+        if(categories == null){
+            return new ArrayList<>();
+        }else{
+            categories.forEach(category -> {
+                result.add(category.getName());
+            });
+        }
         return result;
     }
 }
---
+++ src/test/java/com/nashtech/rookies/java05/AssetManagement/services/impl/AssetServiceImplTest.java
@@ -0,0 +1,20 @@
+package com.nashtech.rookies.java05.AssetManagement.services.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AssetServiceImplTest {
+
+    @Test
+    void getAssetsByPredicates() {
+    }
+
+    @Test
+    void getAllAssetStates() {
+    }
+
+    @Test
+    void getAssetById() {
+    }
+}
