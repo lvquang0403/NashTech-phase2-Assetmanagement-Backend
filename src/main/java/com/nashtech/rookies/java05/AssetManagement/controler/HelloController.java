4328b0c6a1b5feec04e8b1ffe68dbf2743ab1c2e
Duy Nguyen <duy.nguyen@unified.com>
2022-11-20 12:17:13 +0700

    Check deployment

---
+++ src/main/java/com/nashtech/rookies/java05/AssetManagement/controler/HelloController.java
@@ -0,0 +1,15 @@
+package com.nashtech.rookies.java05.AssetManagement.controler;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello spring boot testing edit";
+    }
+}
