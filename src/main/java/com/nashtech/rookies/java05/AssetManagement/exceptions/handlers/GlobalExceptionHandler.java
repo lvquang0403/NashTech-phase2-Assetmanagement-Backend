f0a1ffbaf184b8aaa9d9a9405c446f193b1770ef
Nguyễn Minh Thái <nguyenminhthai3108@gmail.com>
2022-11-24 03:07:06 +0700

    ADD: Handle exception for validation

--- src/main/java/com/nashtech/rookies/java05/AssetManagement/exceptions/handlers/GlobalExceptionHandler.java
+++ src/main/java/com/nashtech/rookies/java05/AssetManagement/exceptions/handlers/GlobalExceptionHandler.java
@@ -7,9 +7,15 @@ import com.nashtech.rookies.java05.AssetManagement.exceptions.RepeatDataExceptio
 import com.nashtech.rookies.java05.AssetManagement.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -48,5 +54,20 @@ public class GlobalExceptionHandler {
     protected ResponseEntity handelForbiddenException(RuntimeException exception) {
         ErrorResponse error = new ErrorResponse(HttpStatus.FORBIDDEN.toString(), exception.getMessage());
         return new ResponseEntity<ErrorResponse>(error, HttpStatus.FORBIDDEN);
+    }
+
+
+    //Handle exception for Validation (@Valid)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        ErrorResponse errors = ErrorResponse.builder().build();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String errorMessage = error.getDefaultMessage();
+            errors.setCode(HttpStatus.BAD_REQUEST.toString());
+            errors.setMessage(errorMessage);
+        });
+        return new ResponseEntity<ErrorResponse>(errors, HttpStatus.BAD_REQUEST);
     }
 }
