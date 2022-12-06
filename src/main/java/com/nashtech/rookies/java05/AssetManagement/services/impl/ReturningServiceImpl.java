e7f7e321ee99b30250153597ad09a947925df1c7
Nguyễn Minh Thái <nguyenminhthai3108@gmail.com>
2022-12-06 19:30:10 +0700

    Create Service and service impl class

---
+++ src/main/java/com/nashtech/rookies/java05/AssetManagement/services/impl/ReturningServiceImpl.java
@@ -0,0 +1,11 @@
+package com.nashtech.rookies.java05.AssetManagement.services.impl;
+
+import com.nashtech.rookies.java05.AssetManagement.services.ReturningService;
+import lombok.Builder;
+import org.springframework.stereotype.Service;
+
+@Builder
+@Service
+public class ReturningServiceImpl implements ReturningService {
+    
+}
