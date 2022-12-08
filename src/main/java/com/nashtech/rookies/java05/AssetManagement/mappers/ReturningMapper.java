29a59633f453125862ddc8aef5a905c0ce963b4d
duy0299 <baoduy0299@gmail.com>
2022-12-08 15:29:18 +0700

    delete @bulder

--- src/main/java/com/nashtech/rookies/java05/AssetManagement/mappers/ReturningMapper.java
+++ src/main/java/com/nashtech/rookies/java05/AssetManagement/mappers/ReturningMapper.java
@@ -20,7 +20,6 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Component
-@Builder
 public class ReturningMapper {
     @Autowired
     private AssignmentRepository assignmentRepository;
