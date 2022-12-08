96017a996482178870e4da76089ef8eacbdc99e8
Nguyễn Minh Thái <nguyenminhthai3108@gmail.com>
2022-12-08 16:54:06 +0700

    Merge to main

--- src/main/java/com/nashtech/rookies/java05/AssetManagement/services/ReturningService.java
+++ src/main/java/com/nashtech/rookies/java05/AssetManagement/services/ReturningService.java
@@ -1,5 +1,6 @@
 package com.nashtech.rookies.java05.AssetManagement.services;
 
+import com.nashtech.rookies.java05.AssetManagement.dtos.request.ReturningRequestCreateDto;
 import com.nashtech.rookies.java05.AssetManagement.dtos.response.APIResponse;
 import com.nashtech.rookies.java05.AssetManagement.dtos.response.ReturningDto;
 
@@ -7,4 +8,6 @@ import java.util.List;
 
 public interface ReturningService {
     APIResponse<List<ReturningDto>> getReturningByPredicates(List<String> states, String returnedDate, String keyword, int page, String orderBy, int locationId);
+
+    public  ReturningDto  create(ReturningRequestCreateDto requestDto);
 }
