892f01c4442a7b5a103d6a3e095d7fd002f94873
Quang Vinh <vinhquang1873@gmail.com>
2022-12-12 08:26:35 +0000

    Merged PR 583: change host that's generated by swagger from HTTP to HTTPS
    change host that's generated by swagger from HTTP to HTTPS

--- src/main/java/com/nashtech/rookies/java05/AssetManagement/config/SwaggerConfiguration.java
+++ src/main/java/com/nashtech/rookies/java05/AssetManagement/config/SwaggerConfiguration.java
@@ -1,12 +1,15 @@
 package com.nashtech.rookies.java05.AssetManagement.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 
 
 @Configuration
@@ -23,5 +26,16 @@ public class SwaggerConfiguration {
                                 .type(SecurityScheme.Type.HTTP)
                                 .scheme("bearer")
                                 .bearerFormat("JWT")));
+    }
+    @Component
+    @Profile("prd")
+    @OpenAPIDefinition(servers = @Server(url = "https://rookie06assetmanagement.azurewebsites.net"))
+    public static class PrdOpenAPIDefinitionConfiguration {
+    }
+
+    @Component
+    @Profile("local")
+    @OpenAPIDefinition(servers = @Server(url = "http://localhost:8080"))
+    public static class LocalOpenAPIDefinitionConfiguration {
     }
 }
